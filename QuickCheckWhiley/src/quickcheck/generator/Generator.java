0fb7c720439639998a5895942606b91cb2a66b2e
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-04-02 22:42:07 +1200

    Integrate IntegerGenerator into Generator file.
    This is to be able to hide the IntegerGenerator and use the Singleton
    pattern, so more IntegerGenerators cannot be created.

--- QuickCheckWhiley/src/quickcheck/generator/Generator.java
+++ QuickCheckWhiley/src/quickcheck/generator/Generator.java
@@ -1,7 +1,48 @@
 package quickcheck.generator;
 
+import java.math.BigInteger;
+import java.util.Random;
+
+import wyil.interpreter.ConcreteSemantics;
 import wyil.interpreter.ConcreteSemantics.RValue;
 
-public interface Generator {
-	public RValue generate();
+public abstract class Generator {
+	/**
+	 * Define generators here
+	 */
+	public static final IntegerGenerator INTEGER_GENERATOR = new IntegerGenerator();
+
+	/**
+	 * Used for generating appropriate values
+	 */
+	private static final ConcreteSemantics semantics = new ConcreteSemantics();
+	
+	/**
+	 * Randomise values produced
+	 */
+	private static Random randomiser = new Random();
+	
+	public abstract RValue generate();
+	
+	/**
+	 * Generate random integer values.
+	 * This uses the singleton pattern, therefore only one generator
+	 * is created.
+	 * 
+	 * @author Janice Chin
+	 *
+	 */
+	private static final class IntegerGenerator extends Generator{		
+		private IntegerGenerator() {
+			
+		}
+		
+		@Override
+		public RValue generate() {
+			// FIXME actually generate a value
+//			BigInteger value = new BigInteger(100, randomiser);
+			BigInteger value = BigInteger.valueOf(5);
+			return Generator.semantics.Int(value);
+		}
+	}
 }

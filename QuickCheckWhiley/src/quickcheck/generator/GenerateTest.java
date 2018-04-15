e9d2aa7c35da4d8354e2f04cd7aa4f4a04014816
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-04-15 16:07:48 +1200

    Change the default way of generating tests.
    This is to be able to handle different test generation techniques.

--- QuickCheckWhiley/src/quickcheck/generator/GenerateTest.java
+++ QuickCheckWhiley/src/quickcheck/generator/GenerateTest.java
@@ -1,5 +1,6 @@
 package quickcheck.generator;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,7 @@ import wyil.interpreter.ConcreteSemantics.RValue;
 
 /**
  * Generate candidate test parameters for a function.
+ * The default is random test generation.
  *  
  * @author Janice Chin
  *
@@ -22,25 +24,34 @@ public class GenerateTest {
 	 * The function/method we want to test
 	 */
 	private Decl.FunctionOrMethod dec;
-	
+
 	/**
 	 *  A list of generators, each corresponding to a parameter in the function/method
 	 */
 	private List<Generator> parameterGenerators;
 
+	// TODO Need to be able to pass multiple arguments? For limits on Integer generators etc?
 	public GenerateTest(FunctionOrMethod dec) {
 		super();
 		this.dec = dec;
 		this.parameterGenerators = new ArrayList<Generator>();
+		createGenerators();
+	}	
+	
+	protected void createGenerators() {
 		// TODO get the generators for each parameter type
-		for(Variable var : dec.getParameters()) {
+		for(Variable var : this.dec.getParameters()) {
 			WhileyFile.Type paramType = var.getType();
 			if(paramType instanceof WhileyFile.Type.Int) {
-				this.parameterGenerators.add(new Generator.IntegerGenerator("-1000", "1000"));
+				this.parameterGenerators.add(new Generator.IntegerGenerator(TestType.RANDOM, BigInteger.valueOf(-1000), BigInteger.valueOf(1000)));
 			}
 			else if(paramType instanceof WhileyFile.Type.Bool) {
-				this.parameterGenerators.add(new Generator.BooleanGenerator());
+				this.parameterGenerators.add(new Generator.BooleanGenerator(TestType.RANDOM));
 			}
+//			else if(paramType instanceof WhileyFile.Type.Array) {
+//				WhileyFile.Type arrEle = ((WhileyFile.Type.Array) paramType).getElement();
+//				// TODO get generator type
+//			}
 		}
 	}
 	
@@ -56,4 +67,23 @@ public class GenerateTest {
 		}
 		return parameters;
 	}
+	
+	// Getters
+	
+	/**
+	 * Get the function/method used for generating the tests
+	 * @return Function/Method
+	 */
+	public Decl.FunctionOrMethod getDec() {
+		return dec;
+	}
+	
+	/**
+	 * Get the generators used for generating test data
+	 * @return Generators corresponding to each parameter type in the function/method
+	 */
+	public List<Generator> getParameterGenerators() {
+		return parameterGenerators;
+	}
+
 }

68c6e69cbe9ff805885055877fac0e6b916b0e11
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-04-30 10:54:29 +1200

    Add null generator

---
+++ QuickCheckWhiley/src/quickcheck/generator/type/NullGenerator.java
@@ -0,0 +1,36 @@
+package quickcheck.generator.type;
+
+import wyil.interpreter.ConcreteSemantics;
+import wyil.interpreter.ConcreteSemantics.RValue;
+
+public class NullGenerator implements Generator{
+	/**
+	 * Used for generating appropriate values
+	 */
+	private static final ConcreteSemantics semantics = new ConcreteSemantics();
+	
+	public NullGenerator() {
+		super();
+	}
+
+	@Override
+	public RValue generate() {
+		return semantics.Null();
+	}
+
+	@Override
+	public int size() {
+		return 1;
+	}
+
+	@Override
+	public void resetCount() {
+		
+	}
+
+	@Override
+	public boolean exceedCount() {
+		return false;
+	}
+
+}

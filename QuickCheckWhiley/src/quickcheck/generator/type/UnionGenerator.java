62cdaff31fdb6135aa49d16cc6941ca73c115101
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-05-01 21:35:29 +1200

    Add union generator

---
+++ QuickCheckWhiley/src/quickcheck/generator/type/UnionGenerator.java
@@ -0,0 +1,80 @@
+package quickcheck.generator.type;
+
+import java.util.List;
+
+import quickcheck.util.TestType;
+import wyil.interpreter.ConcreteSemantics.RValue;
+
+/**
+ * Generate values for the union type
+ * e.g. (int|bool)
+ * would generate integer and boolean values.
+ * 
+ * @author Janice Chin
+ *
+ */
+public final class UnionGenerator implements Generator {
+	private int count = 1;
+	private int size = 0;
+	private TestType testType;
+	private List<Generator> generators;
+	private int currentIndex;
+
+	public UnionGenerator(List<Generator> generators, TestType testType) {
+		this.generators = generators;
+		this.testType = testType;
+		currentIndex = 0;
+		this.size = 0;
+		for(int i=0; i < generators.size(); i++) {
+			size += generators.get(i).size();
+		}
+	}
+
+	@Override
+	public RValue generate() {
+		if(testType == TestType.EXHAUSTIVE) {
+			// Iterate through the different generators
+			if(currentIndex >= generators.size()) {
+				currentIndex = 0;
+			}
+			// Generate all possible values, skip generators that have already finished generating values
+			Generator currentGen = generators.get(currentIndex);
+			while(currentGen.exceedCount()) {
+				currentIndex++;
+				if(currentIndex >= generators.size()) {
+					currentIndex = 0;
+				}
+				currentGen = generators.get(currentIndex);
+			}
+			currentIndex++;
+			count++;
+			return currentGen.generate();
+		}
+		else {
+			// Iterate through the different generators
+			if(currentIndex >= generators.size()) {
+				currentIndex = 0;
+			}
+			return generators.get(currentIndex++).generate();
+		}
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public void resetCount() {
+		count = 1;
+		for(Generator gen : generators) {
+			gen.resetCount();
+		}
+	}
+
+	@Override
+	public boolean exceedCount() {
+		return this.size() < count;
+	}
+
+}

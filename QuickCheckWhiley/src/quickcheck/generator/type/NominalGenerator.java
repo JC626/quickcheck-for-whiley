18a95f277ba7bd3d04fff3b26c878b3cbf4ecda9
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-05-05 21:54:26 +1200

    Add functions to analyse integer ranges.
    Only works for a nominal type wrapping an integer.
    Also must only have one value on the variable side
    e.g. x < 2 and x < 2 + 3 will work but not x + 2 < 5

--- QuickCheckWhiley/src/quickcheck/generator/type/NominalGenerator.java
+++ QuickCheckWhiley/src/quickcheck/generator/type/NominalGenerator.java
@@ -1,7 +1,16 @@
 package quickcheck.generator.type;
 
+import java.math.BigInteger;
+
+import quickcheck.constraints.IntegerRange;
+import wybs.util.AbstractCompilationUnit.Identifier;
+import wybs.util.AbstractCompilationUnit.Tuple;
+import wyc.lang.WhileyFile;
 import wyc.lang.WhileyFile.Decl;
+import wyc.lang.WhileyFile.Expr;
+import wyc.lang.WhileyFile.Expr.VariableAccess;
 import wyil.interpreter.ConcreteSemantics.RValue;
+import wyil.interpreter.Interpreter.CallStack;
 import wyil.interpreter.Interpreter;
 
 /**
@@ -43,9 +52,137 @@ public class NominalGenerator implements Generator{
 			if(i > generator.size()) {
 				// TODO Change this to a different exception
 				throw new Error("No possible values can be generated for the nominal type: " + decl.getName());
+	
+	/**
+	 * Check whether the invariant for a given nominal type holds for this value
+	 * or not. This requires physically evaluating the invariant to see whether
+	 * or not it holds true.
+	 *
+	 * @param var
+	 * @param invariant
+	 * @param instance
+	 * @return
+	 */
+	public void checkInvariant(Generator gen, Decl.Variable var, Tuple<Expr> invariant, Interpreter instance) {
+		// TODO might need dummy value for the nominal type to be able to check invariant.
+		// TODO Only one invariant?
+		if (invariant.size() > 0) {
+			// One or more type invariants to check. Therefore, we need
+			// to execute the invariant and determine whether or not it
+			// returns true.
+			Interpreter.CallStack frame = instance.new CallStack();
+//			frame.putLocal(var.getName(), val);
+			for (int i = 0; i != invariant.size(); ++i) {
+				IntegerRange b = discoverRanges(invariant.get(i), var.getName(), frame, interpreter);
+				if (b != null) {
+					if(gen instanceof IntegerGenerator) {
+						((IntegerGenerator) gen).joinRange(b);
+					}
+				}
 			}
 		}
-		return value;
+	}
+
+	// TODO create own version of executeExpression for the integer ranges
+	/**
+	 * Execute a single expression which is expected to return a single result
+	 * of an expected type. If a result of an incorrect type is returned, then
+	 * an exception is raised.
+	 *
+	 * @param expr
+	 *            The expression to be executed
+	 * @param frame
+	 *            The frame in which the expression is executing
+	 * @return
+	 */
+	public IntegerRange discoverRanges(Expr expr, Identifier nomName, CallStack frame, Interpreter instance) {
+		// TODO check order of operations for equal and not equal
+		RValue val;
+		switch (expr.getOpcode()) {
+		case WhileyFile.EXPR_equal:
+		case WhileyFile.EXPR_notequal:
+			// TODO check type of values on both sides
+//				val = executeNotEqual((Expr.NotEqual) expr, frame);
+			break;
+		case WhileyFile.EXPR_integerlessthan:
+		case WhileyFile.EXPR_integerlessequal:
+		case WhileyFile.EXPR_integergreaterthan:
+		case WhileyFile.EXPR_integergreaterequal:
+			Expr.BinaryOperator binary = (Expr.BinaryOperator) expr;
+			Expr first = binary.getFirstOperand();
+			Expr second = binary.getSecondOperand();
+			
+			// TODO check if field is an integer?
+			BigInteger upperLimit = null;
+			BigInteger lowerLimit = null;
+			/* TODO Would fail for x + 2 < 10 as 
+			 * the lhs is not checked for the extra + 2
+			 * To make the range x < 8 , we want to flip all the 
+			 * operations applied to the x to the other side
+			 * i.e. x + 2 < 10 ==> x < 10 - 2
+			 */
+			// TODO need to know which side the variable is on.
+			if(first instanceof VariableAccess && ((VariableAccess) first).getVariableDeclaration().getName().equals(nomName)){
+				RValue.Int rhs = instance.executeExpression(RValue.Int.class, second, frame);
+				int op = expr.getOpcode();			
+				if(op == WhileyFile.EXPR_integerlessthan) {
+					upperLimit = BigInteger.valueOf(rhs.intValue());
+				}
+				else if(op == WhileyFile.EXPR_integerlessequal) {
+					upperLimit = BigInteger.valueOf(rhs.intValue() + 1);
+				}
+				else if(op == WhileyFile.EXPR_integergreaterthan) {
+					lowerLimit = BigInteger.valueOf(rhs.intValue() + 1);
+				}
+				else if(op == WhileyFile.EXPR_integergreaterequal) {
+					lowerLimit = BigInteger.valueOf(rhs.intValue());
+				}	
+				return new IntegerRange(lowerLimit, upperLimit);
+			}
+			else if(second instanceof VariableAccess && ((VariableAccess) second).getVariableDeclaration().getName().equals(nomName)){
+				RValue.Int lhs = instance.executeExpression(RValue.Int.class, first, frame);
+				int op = expr.getOpcode();
+				if(op == WhileyFile.EXPR_integerlessthan) {
+					lowerLimit = BigInteger.valueOf(lhs.intValue() + 1);
+				}
+				else if(op == WhileyFile.EXPR_integerlessequal) {
+					lowerLimit = BigInteger.valueOf(lhs.intValue());
+				}
+				else if(op == WhileyFile.EXPR_integergreaterthan) {
+					upperLimit = BigInteger.valueOf(lhs.intValue());
+				}
+				else if(op == WhileyFile.EXPR_integergreaterequal) {
+					upperLimit = BigInteger.valueOf(lhs.intValue() + 1);
+				}
+				return new IntegerRange(lowerLimit, upperLimit);
+			}
+			else {
+				// normal expression
+				return null;
+			}
+//			case WhileyFile.EXPR_bitwisenot:
+//				val = executeBitwiseNot((Expr.BitwiseComplement) expr, frame);
+//				break;
+//			case WhileyFile.EXPR_bitwiseor:
+//				val = executeBitwiseOr((Expr.BitwiseOr) expr, frame);
+//				break;
+//			case WhileyFile.EXPR_bitwisexor:
+//				val = executeBitwiseXor((Expr.BitwiseXor) expr, frame);
+//				break;
+//			case WhileyFile.EXPR_bitwiseand:
+//				val = executeBitwiseAnd((Expr.BitwiseAnd) expr, frame);
+//				break;
+//			case WhileyFile.EXPR_bitwiseshl:
+//				val = executeBitwiseShiftLeft((Expr.BitwiseShiftLeft) expr, frame);
+//				break;
+//			case WhileyFile.EXPR_bitwiseshr:
+//				val = executeBitwiseShiftRight((Expr.BitwiseShiftRight) expr, frame);
+//				break;
+		default:
+			// TODO Do we really care about the other invariants?
+			return null;
+		}
+		return null;
 	}
 	
 	@Override

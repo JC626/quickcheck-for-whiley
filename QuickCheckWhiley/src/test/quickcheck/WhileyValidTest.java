4f8757514ba8bc3e8bf089768037edbad7236469
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-05-25 21:31:14 +1200

    Ignore more tests when running Whiley Valid tests

--- QuickCheckWhiley/src/test/quickcheck/WhileyValidTest.java
+++ QuickCheckWhiley/src/test/quickcheck/WhileyValidTest.java
@@ -1,7 +1,5 @@
 package test.quickcheck;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -49,9 +47,9 @@ public class WhileyValidTest {
 	 * Ignored tests and a reason why we ignore them.
 	 */
 	public final static Map<String, String> IGNORED = new HashMap<>();
-
+	
+	// Ignored tests from wyc.testing.AllValidTest and wyc.testing.AllValidVerificationTest
 	static {
-		// Problem Type Checking Union Type
 		IGNORED.put("RecordSubtype_Valid_1", "#696");
 		IGNORED.put("RecordSubtype_Valid_2", "#696");
 		// Function Overloading for Nominal Types
@@ -72,6 +70,90 @@ public class WhileyValidTest {
 		IGNORED.put("ConstrainedReference_Valid_1", "#827");
 		// Unclassified
 		IGNORED.put("Lifetime_Valid_8", "???");
+		// Null Checks as Type Tests in VCG (closed)
+		IGNORED.put("IfElse_Valid_4", "#712");
+		IGNORED.put("Complex_Valid_4", "#712");
+		IGNORED.put("ListAccess_Valid_6", "712");
+		// Verification Condition Generation and Dereference Assignment
+		IGNORED.put("Process_Valid_1", "#743");
+		IGNORED.put("Process_Valid_9", "#743");
+		IGNORED.put("Process_Valid_10", "#743");
+		IGNORED.put("Process_Valid_11", "#743");
+		IGNORED.put("Reference_Valid_2", "#743");
+		IGNORED.put("Reference_Valid_3", "#743");
+		IGNORED.put("Reference_Valid_6", "#743");
+		// Flow Typing and VerificationConditionGeneration
+		IGNORED.put("RecursiveType_Valid_3", "#781");
+		// WyTP Variable Ordering Effect
+		IGNORED.put("ConstrainedList_Valid_26", "#782");
+		// ===============================================================
+		// Whiley Theorem Prover faults
+		// ===============================================================
+		// Issue 2 "Verification of Remainder Operator"
+		IGNORED.put("ConstrainedInt_Valid_22", "WyTP#2");
+		// Issue 12 "Support for Non-linear Arthmetic"
+		IGNORED.put("IntMul_Valid_2", "WyTP#12");
+		IGNORED.put("While_Valid_27", "WyTP#12");
+		IGNORED.put("While_Valid_32", "WyTP#12");
+		// Issue 29 "Triggerless Quantifier Instantiation"
+		IGNORED.put("Assert_Valid_1", "#29");
+		IGNORED.put("ConstrainedList_Valid_14", "WyTP#29");
+		IGNORED.put("ConstrainedList_Valid_18", "WyTP#29");
+		IGNORED.put("Quantifiers_Valid_2", "WyTP#29");
+		// Issue 36 "Support for Division Operator Feature"
+		IGNORED.put("Cast_Valid_5", "WyTP#36");
+		IGNORED.put("IntOp_Valid_1", "WyTP#36");
+		IGNORED.put("IntDiv_Valid_3", "WyTP#36");
+		IGNORED.put("Lambda_Valid_3", "WyTP#36");
+		IGNORED.put("Lambda_Valid_4", "WyTP#36");
+		// Issue 41 "Case Split within Quantifier"
+		IGNORED.put("Property_Valid_4", "WyTP#41");
+		IGNORED.put("Subtype_Valid_5", "WyTP#41");
+		IGNORED.put("RecursiveType_Valid_19", "WyTP#41");
+		// Issue 76 "Casting Record Types"
+		IGNORED.put("Coercion_Valid_9", "WyTP#76");
+		IGNORED.put("RecordCoercion_Valid_1", "WyTP#76");
+		// Issue 80 "(Non-)Empty Type"
+		IGNORED.put("OpenRecord_Valid_4", "WyTP#80");
+		IGNORED.put("OpenRecord_Valid_9", "WyTP#80");
+		// Issue 85 "NegativeArraySizeException in CoerciveSubtypeOperator"
+		IGNORED.put("ConstrainedRecord_Valid_9", "WyTP#85");
+		IGNORED.put("TypeEquals_Valid_54", "WyTP#85");
+		// Issue 89 "Unknown Assertion Failure"
+		IGNORED.put("While_Valid_37", "WyTP#89");
+		// Issue 102 "Support Reference Lifetimes"
+		IGNORED.put("Lifetime_Valid_8", "WyTP#102");
+		// Issue 104 "Incompleteness in CoerciveSubtypeOperator"
+		IGNORED.put("RecursiveType_Valid_7", "WyTP#104");
+		IGNORED.put("While_Valid_15", "WyTP#104");
+		IGNORED.put("While_Valid_20", "WyTP#104");
+		// Issue 107 "Limitation with ReadableRecordExtractor"
+		IGNORED.put("TypeEquals_Valid_30", "WyTP#107");
+		IGNORED.put("TypeEquals_Valid_25", "WyTP#107");
+		// Issue 111 "Infinite Recursive Expansion"
+		IGNORED.put("RecursiveType_Valid_28", "WyTP#111");
+		IGNORED.put("RecursiveType_Valid_29", "WyTP#111");
+		IGNORED.put("Complex_Valid_3", "WyTP#111");
+		IGNORED.put("Complex_Valid_8", "WyTP#111");
+		IGNORED.put("RecursiveType_Valid_2", "WyTP#111");
+		// Issue 112 "More Performance Problems with Type Checking"
+		IGNORED.put("Complex_Valid_2", "WyTP#112");
+		IGNORED.put("BoolList_Valid_3", "WyTP#112");
+		IGNORED.put("While_Valid_2", "WyTP#112");
+		IGNORED.put("While_Valid_26", "WyTP#112");
+		// Issue 114 "Limitation with TypeTestClosure"
+		IGNORED.put("RecursiveType_Valid_4", "WyTP#114");
+		//
+		IGNORED.put("Ensures_Valid_6", "WyTP#133");
+		IGNORED.put("RecursiveType_Valid_22", "WyTP#133");
+		IGNORED.put("While_Valid_24", "WyTP#133");
+		IGNORED.put("While_Valid_34", "#133");
+		IGNORED.put("While_Valid_35", "WyTP#133");
+		//
+		IGNORED.put("String_Valid_6", "??");
+		IGNORED.put("RecursiveType_Valid_12", "??");
+		// Performance problems?
+		IGNORED.put("Complex_Valid_10", "??");
 	}
 
 	// ======================================================================
@@ -108,7 +190,6 @@ public class WhileyValidTest {
 		System.setOut(stream);
 		
 		// Run tests
-		
         try {
             String[] args = new String[] {TEST_DIR + "/" + this.testName, "exhaustive", "100", "-5", "0"};
             QuickCheck.main(args);

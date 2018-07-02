723418debd4cbee7c757451c71a75d58e6b8b88c
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-07-02 20:48:44 +1200

    Remove null operand errors when checking invariants for int ranges

--- QuickCheckWhiley/src/quickcheck/constraints/RangeHelper.java
+++ QuickCheckWhiley/src/quickcheck/constraints/RangeHelper.java
@@ -23,6 +23,8 @@ import wyil.interpreter.Interpreter.CallStack;
  *
  */
 public class RangeHelper {
+	
+	public static final String NULL_INVARIANT_VAR_ERROR = "null operand";
 
 	/**
 	 * Check the ranges on the invariants against the generators.
@@ -70,110 +72,119 @@ public class RangeHelper {
 	 */
 	public static IntegerRange findRange(Expr expr, Identifier name, CallStack frame, Interpreter instance) {
 		IntegerRange range = null;
-		int operator = expr.getOpcode();
-		switch (operator) {
-		case WhileyFile.EXPR_equal:
-			Expr.BinaryOperator binaryEq = (Expr.BinaryOperator) expr;
-			Expr firstEq = binaryEq.getFirstOperand();
-			Expr secondEq = binaryEq.getSecondOperand();
-
-			if(isExpForIntegerRange(firstEq, name)){
-				RValue rhs = instance.executeExpression(RValue.class, secondEq, frame);
-				if(rhs instanceof RValue.Int) {
-					RValue.Int val = (Int) rhs;
-					return new IntegerRange(val.intValue(), val.intValue() + 1);
-				}
-			}
-			else if(isExpForIntegerRange(secondEq, name)){
-				RValue lhs = instance.executeExpression(RValue.class, firstEq, frame);
-				if(lhs instanceof RValue.Int) {
-					RValue.Int val = (Int) lhs;
-					return new IntegerRange(val.intValue(), val.intValue() + 1);
-				}
-			}
-			// normal expression
-			break;
-		case WhileyFile.EXPR_logicalnot:
-			Expr.UnaryOperator unary = (Expr.UnaryOperator) expr;
-			range = findRange(unary.getOperand(), name, frame, instance);
-			// Flip the ranges around
-			if(range != null) {
-				BigInteger upper = range.upperBound() == null ? null : range.upperBound();
-				BigInteger lower = range.lowerBound() == null ? null : range.lowerBound();
-				return new IntegerRange(upper, lower);
-			}
-			break;
-		case WhileyFile.EXPR_logicalor:
-		case WhileyFile.EXPR_logicaland:
-			Expr.NaryOperator nary = (Expr.NaryOperator) expr;
-			Tuple<Expr> operands = nary.getOperands();
-			for(int i=0;i!=operands.size();++i) {
-				IntegerRange other = findRange(operands.get(i), name, frame, instance);
-				if(range == null) {
-					range = other;
-				}
-				else if(other != null) {
-					if(operator == WhileyFile.EXPR_logicalor) {
-						range = range.union(other);
-					}
-					else {
-						range = range.intersection(other);
+		try {
+			int operator = expr.getOpcode();
+			switch (operator) {
+			case WhileyFile.EXPR_equal:
+				Expr.BinaryOperator binaryEq = (Expr.BinaryOperator) expr;
+				Expr firstEq = binaryEq.getFirstOperand();
+				Expr secondEq = binaryEq.getSecondOperand();
+	
+				if(isExpForIntegerRange(firstEq, name)){
+					RValue rhs = instance.executeExpression(RValue.class, secondEq, frame);
+					if(rhs instanceof RValue.Int) {
+						RValue.Int val = (Int) rhs;
+						return new IntegerRange(val.intValue(), val.intValue() + 1);
 					}
 				}
+				else if(isExpForIntegerRange(secondEq, name)){
+					RValue lhs = instance.executeExpression(RValue.class, firstEq, frame);
+					if(lhs instanceof RValue.Int) {
+						RValue.Int val = (Int) lhs;
+						return new IntegerRange(val.intValue(), val.intValue() + 1);
+					}
+				}
+				// normal expression
+				break;
+			case WhileyFile.EXPR_logicalnot:
+				Expr.UnaryOperator unary = (Expr.UnaryOperator) expr;
+				range = findRange(unary.getOperand(), name, frame, instance);
+				// Flip the ranges around
+				if(range != null) {
+					BigInteger upper = range.upperBound() == null ? null : range.upperBound();
+					BigInteger lower = range.lowerBound() == null ? null : range.lowerBound();
+					return new IntegerRange(upper, lower);
+				}
+				break;
+			case WhileyFile.EXPR_logicalor:
+			case WhileyFile.EXPR_logicaland:
+				Expr.NaryOperator nary = (Expr.NaryOperator) expr;
+				Tuple<Expr> operands = nary.getOperands();
+				for(int i=0;i!=operands.size();++i) {
+					IntegerRange other = findRange(operands.get(i), name, frame, instance);
+					if(range == null) {
+						range = other;
+					}
+					else if(other != null) {
+						if(operator == WhileyFile.EXPR_logicalor) {
+							range = range.union(other);
+						}
+						else {
+							range = range.intersection(other);
+						}
+					}
+				}
+				return range;
+			case WhileyFile.EXPR_integerlessthan:
+			case WhileyFile.EXPR_integerlessequal:
+			case WhileyFile.EXPR_integergreaterthan:
+			case WhileyFile.EXPR_integergreaterequal:
+				Expr.BinaryOperator binary = (Expr.BinaryOperator) expr;
+				Expr first = binary.getFirstOperand();
+				Expr second = binary.getSecondOperand();
+	
+				BigInteger upperLimit = null;
+				BigInteger lowerLimit = null;
+				/* TODO Would fail for x + 2 < 10 as
+				 * the lhs is not checked for the extra + 2
+				 * To make the range x < 8 , we want to flip all the
+				 * operations applied to the x to the other side
+				 * i.e. x + 2 < 10 ==> x < 10 - 2
+				 */
+				if(isExpForIntegerRange(first, name)){
+					RValue.Int rhs = instance.executeExpression(RValue.Int.class, second, frame);
+					if(operator == WhileyFile.EXPR_integerlessthan) {
+						upperLimit = BigInteger.valueOf(rhs.intValue());
+					}
+					else if(operator == WhileyFile.EXPR_integerlessequal) {
+						upperLimit = BigInteger.valueOf(rhs.intValue() + 1);
+					}
+					else if(operator == WhileyFile.EXPR_integergreaterthan) {
+						lowerLimit = BigInteger.valueOf(rhs.intValue() + 1);
+					}
+					else if(operator == WhileyFile.EXPR_integergreaterequal) {
+						lowerLimit = BigInteger.valueOf(rhs.intValue());
+					}
+					return new IntegerRange(lowerLimit, upperLimit);
+				}
+				else if(isExpForIntegerRange(second, name)){
+					RValue.Int lhs = instance.executeExpression(RValue.Int.class, first, frame);
+					if(operator == WhileyFile.EXPR_integerlessthan) {
+						lowerLimit = BigInteger.valueOf(lhs.intValue() + 1);
+					}
+					else if(operator == WhileyFile.EXPR_integerlessequal) {
+						lowerLimit = BigInteger.valueOf(lhs.intValue());
+					}
+					else if(operator == WhileyFile.EXPR_integergreaterthan) {
+						upperLimit = BigInteger.valueOf(lhs.intValue());
+					}
+					else if(operator == WhileyFile.EXPR_integergreaterequal) {
+						upperLimit = BigInteger.valueOf(lhs.intValue() + 1);
+					}
+					return new IntegerRange(lowerLimit, upperLimit);
+				}
+				// normal expression
+				break;
+			default:
+				return null;
 			}
-			return range;
-		case WhileyFile.EXPR_integerlessthan:
-		case WhileyFile.EXPR_integerlessequal:
-		case WhileyFile.EXPR_integergreaterthan:
-		case WhileyFile.EXPR_integergreaterequal:
-			Expr.BinaryOperator binary = (Expr.BinaryOperator) expr;
-			Expr first = binary.getFirstOperand();
-			Expr second = binary.getSecondOperand();
-
-			BigInteger upperLimit = null;
-			BigInteger lowerLimit = null;
-			/* TODO Would fail for x + 2 < 10 as
-			 * the lhs is not checked for the extra + 2
-			 * To make the range x < 8 , we want to flip all the
-			 * operations applied to the x to the other side
-			 * i.e. x + 2 < 10 ==> x < 10 - 2
-			 */
-			if(isExpForIntegerRange(first, name)){
-				RValue.Int rhs = instance.executeExpression(RValue.Int.class, second, frame);
-				if(operator == WhileyFile.EXPR_integerlessthan) {
-					upperLimit = BigInteger.valueOf(rhs.intValue());
-				}
-				else if(operator == WhileyFile.EXPR_integerlessequal) {
-					upperLimit = BigInteger.valueOf(rhs.intValue() + 1);
-				}
-				else if(operator == WhileyFile.EXPR_integergreaterthan) {
-					lowerLimit = BigInteger.valueOf(rhs.intValue() + 1);
-				}
-				else if(operator == WhileyFile.EXPR_integergreaterequal) {
-					lowerLimit = BigInteger.valueOf(rhs.intValue());
-				}
-				return new IntegerRange(lowerLimit, upperLimit);
+		}
+		// FIXME another way to check this would be better!
+		catch(RuntimeException e) {
+			if(e.getMessage().equals(NULL_INVARIANT_VAR_ERROR)) {
+				return null;
 			}
-			else if(isExpForIntegerRange(second, name)){
-				RValue.Int lhs = instance.executeExpression(RValue.Int.class, first, frame);
-				if(operator == WhileyFile.EXPR_integerlessthan) {
-					lowerLimit = BigInteger.valueOf(lhs.intValue() + 1);
-				}
-				else if(operator == WhileyFile.EXPR_integerlessequal) {
-					lowerLimit = BigInteger.valueOf(lhs.intValue());
-				}
-				else if(operator == WhileyFile.EXPR_integergreaterthan) {
-					upperLimit = BigInteger.valueOf(lhs.intValue());
-				}
-				else if(operator == WhileyFile.EXPR_integergreaterequal) {
-					upperLimit = BigInteger.valueOf(lhs.intValue() + 1);
-				}
-				return new IntegerRange(lowerLimit, upperLimit);
-			}
-			// normal expression
-			break;
-		default:
-			return null;
+			throw e;
 		}
 		return null;
 	}

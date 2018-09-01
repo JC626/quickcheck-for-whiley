f5f9785310fac4213a92d8114e255fd28456f114
Janice Chin <janice.chin@ecs.vuw.ac.nz>
2018-09-01 22:04:35 +1200

    Forgot to import map

--- QuickCheckWhiley/src/quickcheck/RunTest.java
+++ QuickCheckWhiley/src/quickcheck/RunTest.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import quickcheck.exception.CannotGenerateException;
 import quickcheck.exception.IntegerRangeException;
